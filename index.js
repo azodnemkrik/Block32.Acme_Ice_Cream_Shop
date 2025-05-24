1a0ec24efcc95bdaf8f057fe794515dd9a316d38
Kirk Mendoza <kirkmendoza@gmail.com>
2025-05-24 16:18:08 -0500

    Created db, added place holders for connecting to db

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ app.use(express.json())
 app.use(require('morgan')('dev'))
 
 const init = async () => {
+	// Make Connection
+	await client.connect();
+	console.log("Success! Connected to database.");
+
+	// Define the Table
+	let SQL = `
+
+    `
+
+	// CREATE the Table / Send the initial SQL
+	await client.query(SQL);
+	console.log("Success! Created database Table.");
+
+
+
+	// SEED the Table
+	SQL = `
+
+	`;
+	await client.query(SQL);
+	console.log("Success! Seeded database!");
 
 
 
